d4ac5ad2ff2fa955027d454f5b6a41b71efce88b
SkyWarpX27X <mykhajloost@gmail.com>
2026-03-04 13:48:53 +0200

    Tests for read_file_with_pandas

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,3 +1,5 @@
+import pandas.errors
+
 import app.io.input as inp
 import unittest
 
@@ -13,3 +15,16 @@ class TestReadFile(unittest.TestCase):
     def test_multi_string_file(self):
         result = inp.read_file('tests/file_tests/multi_line.txt')
         self.assertEqual(result, 'This\ntest\nalso\nshould\nbe\nok')
+
+class TestReadFilePandas(unittest.TestCase):
+    def test_file(self):
+        result = inp.read_file_with_pandas('tests/pandas_file_tests/file.txt', ' ')
+        self.assertEqual(result, '101   Petrenko 25   Kyiv\n102  Sydorenko 25   Kyiv\n103 Shevchenko 15 Dnipro')
+
+    def test_csv_file(self):
+        result = inp.read_file_with_pandas('tests/pandas_file_tests/csv_formatted.txt')
+        self.assertEqual(result, '201 Ivanchuk     Kyiv\n202  Fedoriv     Lviv\n203   Kuchma Uzhhorod')
+
+    def test_table_format(self):
+        result = inp.read_file_with_pandas('tests/pandas_file_tests/wrong_format.txt', ' ')
+        self.assertRaises(pandas.errors.ParserError)
